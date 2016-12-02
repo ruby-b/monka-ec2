f7f109ff1b1b9154dbf9b4c070eb4364b2e21400
satomicchy <satomi_t_sora@yahoo.co.jp>
2016-12-02 14:52:04 +0900

    refactoring product layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @books = Book.all
-    render layout: false
+    render layout: 'product'
+  end
+
+  def about
+    render layout: 'product'
   end
 end
