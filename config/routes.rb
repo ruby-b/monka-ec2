8e89b7487993084168a2031cbbadc20dcf9112cb
d-kawabata <d.kawabata98@gmail.com>
2016-12-28 11:42:50 +0900

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :orders, only: [:new, :create, :show]
+  resources :orders, only: [:new, :create]
   resources :musics
   get 'products/index'
   get 'about', to: 'products#about'
