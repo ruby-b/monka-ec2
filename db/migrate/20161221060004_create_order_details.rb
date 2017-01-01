92bb1ca2b21722c2269c31b4afba8c78042a5973
dkawabata <d.kawabata98@gmail.com>
2017-01-02 03:06:06 +0900

    ・add 'has_many :orders' to user.rb ・change belongs_to to references

--- db/migrate/20161221060004_create_order_details.rb
+++ db/migrate/20161221060004_create_order_details.rb
@@ -1,8 +1,8 @@
 class CreateOrderDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :order_details do |t|
-      t.belongs_to :order, foreign_key: true
-      t.belongs_to :product, foreign_key: true
+      t.references :order, foreign_key: true
+      t.references :product, foreign_key: true
 
       t.timestamps
     end
